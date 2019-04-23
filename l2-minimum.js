fae08f693e3964d18465d908a716688f2c33844b
joanpadolina <34737850+joanpadolina@users.noreply.github.com>
2019-04-23 15:00:37 +0200

    Create l2-minimum.js

---
+++ l2-minimum.js
@@ -0,0 +1,7 @@
+// Minimmum
+let minWaarde = function(a, b) {      //parameters a b
+  return Math.min(a, b);              // geef de laagste cijfer terug
+
+}
+
+console.log(minWaarde(2, 90));        // geeft argumenten door naar de functie
